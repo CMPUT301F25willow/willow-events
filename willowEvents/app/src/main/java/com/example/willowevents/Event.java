89995ec652f2dbc7248c008227c4148779c2abd9
Michelle Tran <mttran@ualberta.ca>
2025-10-28 12:08:35 -0600

    Added a bit to Events.java

--- willowEvents/app/src/main/java/com/example/willowevents/Event.java
+++ willowEvents/app/src/main/java/com/example/willowevents/Event.java
@@ -2,8 +2,23 @@ package com.example.willowevents;
 
 public class Event {
     private String name;
+    private String details;
+    private String lotteryDetails = "This is lottery details which is seen in a scrollable form";
+    private Boolean location;   // figure soemthing out
+
+    // private Time deadline; soemthing
+
+   // private WaitList waitlist;
+    // private CancelList cancellist;
+    // private AproveList aprovelist;
+
+
 
     public Event(String name){
+        this.name = name;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
 
@@ -11,7 +26,10 @@ public class Event {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+
+    public String getDetails(){ return details;};
+
+    public String getLottery(){ return lotteryDetails;};
+    
+
 }
