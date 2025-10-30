a2fa8f3f9855c7db17d158813c2b639ff9e6ade9
Taylor Crawford <123581249+dngrjeans@users.noreply.github.com>
2025-10-30 14:17:01 -0600

    activity_event_organizer_entrants_view rough (functional) draft

--- willowEvents/app/src/main/java/com/example/willowevents/MainActivity.java
+++ willowEvents/app/src/main/java/com/example/willowevents/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 //      FOR TESTING:
-        Intent myIntent = new Intent(MainActivity.this, EventCreationView.class);
+        Intent myIntent = new Intent(MainActivity.this, EventOrganizerView.class);
         startActivity(myIntent);
 
     }
