457127d9ad5f0bd5acb6202c36b9618bdb726129
msheepy <monelson@ualberta.ca>
2025-11-01 08:43:10 -0600

    ValidateData class for functions to make sure user input is valid and implemented waitlist limit buttons in Event Creation View

--- willowEvents/app/src/main/java/com/example/willowevents/EventCreationView.java
+++ willowEvents/app/src/main/java/com/example/willowevents/EventCreationView.java
@@ -1,6 +1,10 @@
 package com.example.willowevents;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class EventCreationView extends AppCompatActivity {
+
+    private EditText sizeLimit;
+    private CheckBox doLimit;
+    private boolean limiting = false;
+    private String waitListLimit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +28,31 @@ public class EventCreationView extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        sizeLimit = findViewById(R.id.waitlist_size_limit_entry);
+        doLimit = findViewById(R.id.limit_waitlist_checkbox);
+
+        doLimit.setOnClickListener(view -> {
+            limiting = true;
+        });
+
+        sizeLimit.setOnClickListener(view -> {
+            if(limiting){
+                //I will have to implement this somewhere else
+                //Need to fix this
+                sizeLimit.setFocusable(true);
+                sizeLimit.setFocusableInTouchMode(true);
+                waitListLimit = sizeLimit.getText().toString();
+                //Make sure user input is valid
+                if(!ValidateData.containsOnlyDigits(waitListLimit)){
+                    //don't let them
+                    sizeLimit.setText("");
+                }
+            }
+            else {
+                sizeLimit.setFocusable(false);
+                sizeLimit.setFocusableInTouchMode(false);
+            }
+        });
     }
-
-
-
-
 }
