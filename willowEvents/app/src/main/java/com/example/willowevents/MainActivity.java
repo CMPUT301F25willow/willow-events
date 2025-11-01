8a85d309a94da422b731b3cef17b7c60066a8095
msheepy <monelson@ualberta.ca>
2025-10-31 20:35:14 -0600

    Cleaning up some errors

--- willowEvents/app/src/main/java/com/example/willowevents/MainActivity.java
+++ willowEvents/app/src/main/java/com/example/willowevents/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 //      FOR TESTING:
-        Intent myIntent = new Intent(MainActivity.this, InitialView.class);
+        Intent myIntent = new Intent(MainActivity.this, EntrantHomeView.class);
         startActivity(myIntent);
 
     }
