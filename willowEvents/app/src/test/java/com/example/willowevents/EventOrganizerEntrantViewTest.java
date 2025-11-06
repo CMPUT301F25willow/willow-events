ef1f63b1843f5eea954ca3393dbc2c73baf81e1d
Michelle Tran <mttran@ualberta.ca>
2025-11-06 16:48:28 -0700

    more tests

---
+++ willowEvents/app/src/test/java/com/example/willowevents/EventOrganizerEntrantViewTest.java
@@ -0,0 +1,38 @@
+package com.example.willowevents;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import com.example.willowevents.model.Entrant;
+import com.example.willowevents.model.Event;
+
+import java.util.ArrayList;
+public class EventOrganizerEntrantViewTest {
+
+    private Event mockEvent(){
+
+        Event event = new Event("EventOne");
+
+         ArrayList<String> tempList = new ArrayList<>();
+         ArrayList<Entrant> entrantlist = new ArrayList<>();
+
+         entrantlist.add(new Entrant("0", "userOne", "email@gmail.com", "306 123 456", tempList));
+         entrantlist.add(new Entrant("1", "userTwo", "email@gmail.com", "306 123 456", tempList));
+         entrantlist.add(new Entrant("2", "userThree", "email@gmail.com", "306 123 456", tempList));
+         entrantlist.add(new Entrant("3", "userFour", "email@gmail.com", "306 123 456", tempList));
+
+         event.setWaitlist(entrantlist);
+        return event;
+    }
+
+
+
+
+
+
+}
