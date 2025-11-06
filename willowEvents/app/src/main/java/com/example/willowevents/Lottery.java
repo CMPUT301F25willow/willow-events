4daf3f9776409f1dc2b9ec110264a0f0e44df5ac
Michelle Tran <mttran@ualberta.ca>
2025-11-05 17:20:21 -0700

    add

--- willowEvents/app/src/main/java/com/example/willowevents/Lottery.java
+++ willowEvents/app/src/main/java/com/example/willowevents/Lottery.java
@@ -1,9 +1,31 @@
 package com.example.willowevents;
 
+import java.util.ArrayList;
+
 public class Lottery {
 
     // this is lottery system
     // it's a class cause it's probably easier that way
+
+    private ArrayList<User> selected;
+    public ArrayList<User> doLottery(ArrayList<User> waitList, int max){
+
+
+        while ( max > 0){
+
+            max-=1;
+            int size = waitList.size();
+
+
+        }
+
+
+        return selected;
+    }
+
+
+
+
 
 
 
