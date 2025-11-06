996f80826707554841db8b898606e31b1e8ec60f
Michelle Tran <mttran@ualberta.ca>
2025-11-06 16:48:28 -0700

    more tests

--- willowEvents/app/src/test/java/com/example/willowevents/EventOrganizerEntrantViewTest.java
+++ willowEvents/app/src/test/java/com/example/willowevents/EventOrganizerEntrantViewTest.java
@@ -10,36 +10,26 @@ import static org.junit.Assert.assertTrue;
 
 import com.example.willowevents.model.Entrant;
 import com.example.willowevents.model.Event;
-import com.example.willowevents.model.Organizer;
 
 import java.util.ArrayList;
 public class EventOrganizerEntrantViewTest {
 
-    private Event mockEvent(String name){
+    private Event mockEvent(){
 
-        Event event = new Event(name);
+        Event event = new Event("EventOne");
 
          ArrayList<String> tempList = new ArrayList<>();
          ArrayList<Entrant> entrantlist = new ArrayList<>();
 
-         entrantlist.add(new Entrant("0", "userOne", "email@gmail.com", "306 123 456", "type", tempList));
-         entrantlist.add(new Entrant("1", "userTwo", "email@gmail.com", "306 123 456", "type", tempList));
-         entrantlist.add(new Entrant("2", "userThree", "email@gmail.com", "306 123 456","type", tempList));
-         entrantlist.add(new Entrant("3", "userFour", "email@gmail.com", "306 123 456", "type",tempList));
+         entrantlist.add(new Entrant("0", "userOne", "email@gmail.com", "306 123 456", tempList));
+         entrantlist.add(new Entrant("1", "userTwo", "email@gmail.com", "306 123 456", tempList));
+         entrantlist.add(new Entrant("2", "userThree", "email@gmail.com", "306 123 456", tempList));
+         entrantlist.add(new Entrant("3", "userFour", "email@gmail.com", "306 123 456", tempList));
 
          event.setWaitlist(entrantlist);
         return event;
     }
 
-
-    private Organizer mockOrganizer(){
-        ArrayList<String> tempList = new ArrayList<>();
-        Organizer organizer = new Organizer("0", "userOne", "email@gmail.com", "306 123 456", "type", tempList);
-        organizer.getCreatedList().add(mockEvent("event1"));
-        return organizer;
-    }
-
-    
 
 
 
