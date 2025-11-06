dd2332404b0ea47e1a2351b6485e9042a07089b2
jereves <sinfuego@ualberta.ca>
2025-11-06 14:03:41 -0700

    add update functionality for user in database TODO: update profile

--- willowEvents/app/src/main/java/com/example/willowevents/ProfileView.java
+++ willowEvents/app/src/main/java/com/example/willowevents/ProfileView.java
@@ -1,6 +1,7 @@
 package com.example.willowevents;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,10 @@ public class ProfileView extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button editButton = findViewById(R.id.edit_button);
+
+//        editButton.on
+
     }
 }
