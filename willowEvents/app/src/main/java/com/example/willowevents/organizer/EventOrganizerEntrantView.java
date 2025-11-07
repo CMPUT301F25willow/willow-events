3dfd8cc45ca56f9a4021b021d106300261f85fd1
Michelle Tran <mttran@ualberta.ca>
2025-11-06 17:55:12 -0700

    commit
    # Conflicts:
    #	willowEvents/.idea/deploymentTargetSelector.xml
    #	willowEvents/app/src/main/java/com/example/willowevents/organizer/MainOrganizerView.java
    
    # Conflicts:
    #	willowEvents/app/src/main/java/com/example/willowevents/model/Organizer.java
    #	willowEvents/app/src/test/java/com/example/willowevents/EventTest.java

--- willowEvents/app/src/main/java/com/example/willowevents/organizer/EventOrganizerEntrantView.java
+++ willowEvents/app/src/main/java/com/example/willowevents/organizer/EventOrganizerEntrantView.java
@@ -231,10 +231,11 @@ public class EventOrganizerEntrantView extends AppCompatActivity {
         ArrayList<String> tempList = new ArrayList<>();
         ArrayList<Entrant> entrantlist = new ArrayList<>();
 
-        entrantlist.add(new Entrant("0", "userOne", "email@gmail.com", "306 123 456", tempList));
-        entrantlist.add(new Entrant("1", "userTwo", "email@gmail.com", "306 123 456", tempList));
-        entrantlist.add(new Entrant("2", "userThree", "email@gmail.com", "306 123 456", tempList));
-        entrantlist.add(new Entrant("3", "userFour", "email@gmail.com", "306 123 456", tempList));
+        entrantlist.add(new Entrant("0", "userOne", "email@gmail.com", "306 123 456", "type", tempList));
+        entrantlist.add(new Entrant("1", "userTwo", "email@gmail.com", "306 123 456", "type", tempList));
+        entrantlist.add(new Entrant("2", "userThree", "email@gmail.com", "306 123 456","type", tempList));
+        entrantlist.add(new Entrant("3", "userFour", "email@gmail.com", "306 123 456", "type",tempList));
+
 
         event.setWaitlist(entrantlist);
         return event;*/
--- willowEvents/app/src/test/java/com/example/willowevents/EventOrganizerEntrantViewTest.java
+++ willowEvents/app/src/test/java/com/example/willowevents/EventOrganizerEntrantViewTest.java
@@ -10,26 +10,36 @@ import static org.junit.Assert.assertTrue;
 
 import com.example.willowevents.model.Entrant;
 import com.example.willowevents.model.Event;
+import com.example.willowevents.model.Organizer;
 
 import java.util.ArrayList;
 public class EventOrganizerEntrantViewTest {
 
-    private Event mockEvent(){
+    private Event mockEvent(String name){
 
-        Event event = new Event("EventOne");
+        Event event = new Event(name);
 
          ArrayList<String> tempList = new ArrayList<>();
          ArrayList<Entrant> entrantlist = new ArrayList<>();
 
-         entrantlist.add(new Entrant("0", "userOne", "email@gmail.com", "306 123 456", tempList));
-         entrantlist.add(new Entrant("1", "userTwo", "email@gmail.com", "306 123 456", tempList));
-         entrantlist.add(new Entrant("2", "userThree", "email@gmail.com", "306 123 456", tempList));
-         entrantlist.add(new Entrant("3", "userFour", "email@gmail.com", "306 123 456", tempList));
+         entrantlist.add(new Entrant("0", "userOne", "email@gmail.com", "306 123 456", "type", tempList));
+         entrantlist.add(new Entrant("1", "userTwo", "email@gmail.com", "306 123 456", "type", tempList));
+         entrantlist.add(new Entrant("2", "userThree", "email@gmail.com", "306 123 456","type", tempList));
+         entrantlist.add(new Entrant("3", "userFour", "email@gmail.com", "306 123 456", "type",tempList));
 
          event.setWaitlist(entrantlist);
         return event;
     }
 
+
+    private Organizer mockOrganizer(){
+        ArrayList<String> tempList = new ArrayList<>();
+        Organizer organizer = new Organizer("0", "userOne", "email@gmail.com", "306 123 456", "type", tempList);
+        organizer.getCreatedList().add(mockEvent("event1"));
+        return organizer;
+    }
+
+    
 
 
 
