f147eccc44f0d17b02a4f23d8a14771852ed22dd
jereves <sinfuego@ualberta.ca>
2025-11-06 19:31:15 -0700

    checkpoint with eventController + javadoc

--- willowEvents/app/src/main/java/com/example/willowevents/EventController.java
+++ willowEvents/app/src/main/java/com/example/willowevents/EventController.java
@@ -1,4 +1,106 @@
 package com.example.willowevents;
 
+import androidx.annotation.Nullable;
+
+import com.example.willowevents.model.Event;
+import com.example.willowevents.model.User;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Objects;
+
 public class EventController {
+    private FirebaseFirestore eventDB;
+    private CollectionReference eventsRef;
+
+
+    // get all users
+    public EventController() {
+            // get pointer to database
+            eventDB = FirebaseFirestore.getInstance();
+            // get all users
+            eventsRef = eventDB.collection("events");
+
     }
+    public void retrieveCapacity(String eventID, OnExistsCapacity callback) {
+        DocumentReference docRef = eventsRef.document(eventID);
+        docRef.get().addOnSuccessListener(document -> {
+
+            Long waitlistLimit = null;
+            if (document.exists()) {
+                 waitlistLimit = document.getLong("waitlistLimit");
+            }
+            callback.getCapacity(waitlistLimit);
+        });
+    }
+
+    // ------------- ADDING FUNCTIONALITY
+    /**
+     * Adds user to the waitlist
+     * @param eventID the ID of the event
+     * @param userID the ID of the user
+     */
+    public void addUserWaitlist(String eventID, String userID) {
+        eventsRef.document(eventID).update("waitlist", FieldValue.arrayUnion(userID));
+    }
+
+    /**
+     * Adds user to the inviteList
+     * @param eventID
+     */
+    public void addUserInvitelist(String eventID, String userID) {
+        eventsRef.document(eventID).update("inviteList", FieldValue.arrayUnion(userID));
+    }
+
+    /**
+     * Adds user to the registeredList
+     * @param userID the ID of the user
+     * @param eventID the ID of the event
+     *
+     */
+    public void addUserRegisteredList(String eventID, String userID) {
+        eventsRef.document(eventID).update("approvedList", FieldValue.arrayUnion(userID));
+    }
+
+    /**
+     * Adds user to the cancelled list
+     * @param userID the ID of the user
+     * @param eventID the ID of the event
+     */
+    public void addUserCancelledList(String eventID, String userID) {
+        eventsRef.document(eventID).update("cancelledList", FieldValue.arrayUnion(userID));
+    }
+    //---------------- DELETING FUNCTIONALITY
+
+
+
+    //---------------- EXISTS-IN-ARRAY FUNCTIONALITY
+
+    /**
+     * Checks if a user exists in as given array
+     * @param eventID the string of the device ID of the user
+     */
+    public void existsInGivenArray(String eventID, String arrayName, String user) {
+
+    }
+
+
+
+
+
+    public interface OnExistsCapacity {
+        void getCapacity(Long capacity);
+    }
+    public interface ExistsInArray {
+
+    void existsInArray(boolean contains);
+    }
+}
+
+
+
+
+
+
