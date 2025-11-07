9adb77160a8f5b1c56f2efad07022588049f4749
Adrian <philaibarra@gmail.com>
2025-11-07 16:39:29 -0700

    -Available Events now filters the events based on spacial availability -needs test cases

--- willowEvents/app/src/main/java/com/example/willowevents/entrant/EntrantHomeView.java
+++ willowEvents/app/src/main/java/com/example/willowevents/entrant/EntrantHomeView.java
@@ -2,6 +2,8 @@ package com.example.willowevents.entrant;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
