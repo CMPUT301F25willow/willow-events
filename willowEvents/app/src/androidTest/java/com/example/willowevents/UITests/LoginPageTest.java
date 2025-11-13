9b297271590c045f62e64ae9185d594b528e4af7
msheepy <monelson@ualberta.ca>
2025-11-13 10:32:31 -0600

    CLose but database doesn't seem to be connecting

--- willowEvents/app/src/androidTest/java/com/example/willowevents/UITests/LoginPageTest.java
+++ willowEvents/app/src/androidTest/java/com/example/willowevents/UITests/LoginPageTest.java
@@ -1,4 +1,4 @@
-package com.example.willowevents;
+package com.example.willowevents.UITests;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
@@ -9,20 +9,29 @@ import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import android.content.Context;
 import android.provider.Settings;
 
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 
+import com.example.willowevents.InitialView;
+import com.example.willowevents.R;
+import com.example.willowevents.SelectRoleView;
+import com.example.willowevents.UserController;
 import com.example.willowevents.entrant.EntrantHomeView;
 import com.example.willowevents.organizer.MainOrganizerView;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -32,6 +41,7 @@ public class LoginPageTest {
     private final FirebaseFirestore db = FirebaseFirestore.getInstance();
     Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
     String deviceID = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
+    UserController userController = new UserController();
 
     private Map<String, Object> addUser(String type){
         Map<String, Object> user = new HashMap<>();
@@ -53,70 +63,80 @@ public class LoginPageTest {
     }
 
     @Rule
-    public ActivityScenarioRule<InitialView> scenario = new ActivityScenarioRule<InitialView>(InitialView.class);
+    public IntentsTestRule<InitialView> intentsTestRule  = new IntentsTestRule<>(InitialView.class);
     @Test
     public void testLoginOrganizer() {
-        //Set up emulator db
-        db.useEmulator("10.0.2.2", 8080);
-        clearCollection("users");
-        db.collection("users").add(addUser("organizer"));
+        db.collection("users").document(deviceID)
+            .get()
+            .addOnSuccessListener(documentSnapshot -> {
+                if (!documentSnapshot.exists()) {
+                    userController.addNewOrganizerUser(deviceID);
+                }
+            })
+            .addOnFailureListener(e -> {
 
+            });
         onView(ViewMatchers.withId(R.id.login_button)).perform(click());
         intended(hasComponent(MainOrganizerView.class.getName()));
     }
     @Test
     public void testLoginEntrant() {
-        db.useEmulator("10.0.2.2", 8080);
-        clearCollection("users");
-        db.collection("users").add(addUser("entrant"));
-
+        db.collection("users").document(deviceID)
+            .get()
+            .addOnSuccessListener(documentSnapshot -> {
+                if (!documentSnapshot.exists()) {
+                    userController.addNewEntrantUser(deviceID);
+                }
+            });
         onView(withId(R.id.login_button)).perform(click());
         intended(hasComponent(EntrantHomeView.class.getName()));
     }
     @Test
-    public void testLoginWithDeviceIdOrganizer() {
-        //Add the user to the database
-        db.useEmulator("10.0.2.2", 8080);
-        clearCollection("users");
-        db.collection("users").add(addUser("organizer"));
-        //click button
+    public void testWLoginWithDeviceIdOrganizer() {
+        db.collection("users").document(deviceID)
+            .get()
+            .addOnSuccessListener(documentSnapshot -> {
+                if (!documentSnapshot.exists()) {
+                    userController.addNewOrganizerUser(deviceID);
+                }
+            });
         onView(withId(R.id.device_login_button)).perform(click());
-        //assert switched to appropriate activity
         intended(hasComponent(MainOrganizerView.class.getName()));
-        //make sure added to database???
     }
     @Test
     public void testLoginWithDeviceIdEntrant() {
-        //Add the user to the database
-        db.useEmulator("10.0.2.2", 8080);
-        clearCollection("users");
-        db.collection("users").add(addUser("entrant"));
-        //click button
+        db.collection("users").document(deviceID)
+            .get()
+            .addOnSuccessListener(documentSnapshot -> {
+                if (!documentSnapshot.exists()) {
+                    userController.addNewEntrantUser(deviceID);
+                }
+            });
         onView(withId(R.id.device_login_button)).perform(click());
-        //assert switched to appropriate activity
         intended(hasComponent(EntrantHomeView.class.getName()));
-        //make sure added to database???
     }
     @Test
     public void testSignup() {
-        //Set up db
-        db.useEmulator("10.0.2.2", 8080);
-        clearCollection("users");
-        //click button
+        db.collection("users").document(deviceID)
+                .get()
+                .addOnSuccessListener(documentSnapshot -> {
+                    if (documentSnapshot.exists()) {
+                        db.collection("users").document(deviceID).delete();
+                    }
+                });
         onView(withId(R.id.signup_button)).perform(click());
-        //make sure transitioned to the correct page
         intended(hasComponent(SelectRoleView.class.getName()));
-        //make sure added to database???
     }
     @Test
     public void testSignupWithDeviceId() {
-        //set up db
-        db.useEmulator("10.0.2.2", 8080);
-        clearCollection("users");
-        //click button
+        db.collection("users").document(deviceID)
+            .get()
+            .addOnSuccessListener(documentSnapshot -> {
+                if (documentSnapshot.exists()) {
+                    db.collection("users").document(deviceID).delete();
+                }
+            });
         onView(withId(R.id.device_signup_button)).perform(click());
-        //make sure transitioned to the correct page
         intended(hasComponent(SelectRoleView.class.getName()));
-        //make sure added to database???
     }
 }
