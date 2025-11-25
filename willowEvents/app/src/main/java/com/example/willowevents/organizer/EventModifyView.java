62aa5464de1f8a507b9358685e504a6dd5546f86
Michelle Tran <mttran@ualberta.ca>
2025-11-25 16:05:26 -0700

    Finished updatePoster, worked on some frontend for EventModifyView, finished frontend for updatePosterFragment

--- willowEvents/app/src/main/java/com/example/willowevents/organizer/EventModifyView.java
+++ willowEvents/app/src/main/java/com/example/willowevents/organizer/EventModifyView.java
@@ -2,6 +2,7 @@ package com.example.willowevents.organizer;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -12,6 +13,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.willowevents.CreateEventUseCase;
 import com.example.willowevents.R;
 import com.example.willowevents.ValidateData;
 import com.example.willowevents.model.Event;
@@ -36,11 +38,61 @@ import java.util.Locale;
  */
 public class EventModifyView extends AppCompatActivity {
 
-    /**
-     * Effectively does the same thing as EventCreationView..
-     *//*
+    private EditText nameEt, descEt, waitlistLimitEt;
+    private TextView eventDateTv, regOpenTv, regCloseTv;
+    private Button eventDateBtn, regOpenBtn, regCloseBtn, finishEditBtn, cancelBtn;
+    private CheckBox limitWaitlistCb;
+    private final SimpleDateFormat displayFmt = new SimpleDateFormat("M/d/yy|HH:mm", Locale.US);
 
 
+    @Nullable private Date eventDate;
+    @Nullable private Date regOpenDate;
+    @Nullable private Date regCloseDate;
+
+    // Use Case handles the actual event Creation
+    private CreateEventUseCase useCase;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_edit_view);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Intent origIntent = new Intent(this, EventOrganizerInfoView.class);
+        Bundle extras = getIntent().getExtras();
+
+        bindViews();
+
+        cancelBtn.setOnClickListener(view -> {
+            Intent myIntent = new Intent(EventModifyView.this, EventOrganizerInfoView.class);
+            startActivity(myIntent);
+        });
+
+
+
+    }
+
+
+    private void bindViews() {
+        nameEt = findViewById(R.id.event_name_entry);
+        descEt = findViewById(R.id.additional_details_entry);
+        waitlistLimitEt = findViewById(R.id.waitlist_size_limit_entry);
+
+        eventDateTv = findViewById(R.id.event_start_date);
+        regOpenTv = findViewById(R.id.registration_open_date);
+        regCloseTv = findViewById(R.id.registration_deadline_date);
+
+        eventDateBtn = findViewById(R.id.event_start_date_button);
+        regOpenBtn = findViewById(R.id.registration_open_button);
+        regCloseBtn = findViewById(R.id.registration_deadline_button);
+
+        cancelBtn = findViewById(R.id.create_event_cancel_button);
+        finishEditBtn = findViewById(R.id.create_event_create_button);
+
+        limitWaitlistCb = findViewById(R.id.limit_waitlist_checkbox);
+    }
 
 }
 
