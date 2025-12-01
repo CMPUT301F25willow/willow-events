bdba6485ef961ab11ea0dfe2a1a2049322887390
Michelle Tran <mttran@ualberta.ca>
2025-12-01 07:17:07 -0700

    Worked on EventOrganizerInfoView, we might not need EventModifyView

--- willowEvents/app/src/main/java/com/example/willowevents/organizer/EventOrganizerInfoView.java
+++ willowEvents/app/src/main/java/com/example/willowevents/organizer/EventOrganizerInfoView.java
@@ -4,12 +4,14 @@ import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,7 +19,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.bumptech.glide.Glide;
 import com.example.willowevents.R;
+import com.example.willowevents.controller.EventController;
+import com.example.willowevents.entrant.EventEntrantView;
 import com.example.willowevents.model.Event;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -27,33 +32,76 @@ import com.google.firebase.firestore.FirebaseFirestore;
  */
 public class EventOrganizerInfoView extends AppCompatActivity {
     private String eventId;
-    private Event event;
     private Button backButton, seeEntrants, uploadImage, editEvent;
+    private TextView time, details, heading;
+    private ImageView posterImage;
+    private Event currentEvent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_organizer_info_view);
-        //FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-        backButton = findViewById(R.id.back_button);
-        seeEntrants = findViewById(R.id.entrants_button);
-        uploadImage = findViewById(R.id.upload_image_button);
-        editEvent = findViewById(R.id.edit_button);
 
-        Intent origIntent = new Intent(this, EventOrganizerEntrantView.class);
+
+
         //check for any data sent along side activity change
+
         Bundle extras = getIntent().getExtras();
-        if (extras != null) {
-            eventId = extras.getString("Event ID");
-            if (eventId == null) {
-                android.widget.Toast.makeText(this, "Missing event ID", android.widget.Toast.LENGTH_LONG).show();
-                startActivity(origIntent);
-            }
-        } else {
+        eventId = extras.getString("Event ID");
+        if (eventId == null || eventId.trim().isEmpty()) {
             android.widget.Toast.makeText(this, "Missing event ID", android.widget.Toast.LENGTH_LONG).show();
-            startActivity(origIntent);
+            finish();
+            return;
         }
+
+        bindViews();
+
+
+        db.collection("events").document(eventId).get().addOnSuccessListener(snapshot -> {
+            if (!snapshot.exists()){
+                return;
+            }
+            currentEvent = snapshot.toObject(Event.class);
+
+            String title = currentEvent.getTitle() + "\n"
+                    + currentEvent.getEventDate();
+            time.setText(title);
+
+            if (currentEvent == null){
+                return;
+            }
+
+            // DISPLAY POSTER IMAGE IF EXISTS, COLLAPSE IF DOESNT
+            String posterUrl = currentEvent.getPosterUrl();
+            if (posterUrl != null && !posterUrl.isEmpty()) {
+                Glide.with(this)
+                        .load(posterUrl)
+                        .into(posterImage);
+            } else {
+                posterImage.setVisibility(View.GONE);
+            }
+
+            details.setText(currentEvent.getDescription());
+
+            /**
+            //update the event back into firestore
+            db.collection("events")
+                    .document(eventId)
+                    .update(
+                            "waitlist", event.getWaitlist(),
+                            "inviteList", event.getInviteList(),
+                            "approvedList", event.getApprovedList(),
+                            "invitelistlimit", event.getInvitelistlimit()
+                    );
+                **/
+        });
+
+
+
+
+
 
         //TODO: event = get event from database with eventId
 
@@ -81,6 +129,7 @@ public class EventOrganizerInfoView extends AppCompatActivity {
             myIntent.putExtra("Event ID", eventId);
             startActivity(myIntent);
         });
+
 
 
 
@@ -139,4 +188,31 @@ public class EventOrganizerInfoView extends AppCompatActivity {
         });
 
     }
+
+    /**
+     * Binds member fields to their corresponding views in the layout.
+     * <p>
+     * This method should be called after {@link #setContentView(int)} so that
+     * {@link #findViewById(int)} can locate the views defined in XML.
+     */
+    private void bindViews() {
+
+        time = findViewById(R.id.event_info);
+        backButton = findViewById(R.id.back_button);
+        seeEntrants = findViewById(R.id.entrants_button);
+        uploadImage = findViewById(R.id.upload_image_button);
+        posterImage  = findViewById(R.id.uploaded_image);
+        details = findViewById(R.id.event_details);
+
+        details.setMovementMethod(new ScrollingMovementMethod());
+
+        // Not in use
+        editEvent = findViewById(R.id.edit_button);
+        editEvent.setVisibility(View.GONE);
+
+    }
+
+
+
+
 }
